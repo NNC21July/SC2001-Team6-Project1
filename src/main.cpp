8d21a3afe3bb153fccb4763a8aa442e374c3deff
marcustwle <tanwenle2014@gmail.com>
2026-09-13 02:15:05 +0800

    Modified main.cpp to run experiments on large datasets read from files and test different values of S.

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,55 @@
 #include "hybridSort.hpp"
 #include <iostream>
+#include <vector>
+#include <fstream>   
+#include <string>    
+#include <chrono>    
+
 using ll = long long;
 using namespace std;
 
-int main()
-{
+int main(int argc, char* argv[]) {
+    // This program expects 3 arguments:
+    // 1. The program name (e.g., my_sorter.exe)
+    // 2. The input file name (e.g., dataset_10000.txt)
+    // 3. The threshold value 'S' (e.g., 32)
 
-    vector<int> sample = {3, 7, 8, 1, 2, 0, 4, 6};
+    if (argc != 3) {
+        cerr << "Usage: " << argv[0] << " <input_filename> <S_value>" << endl;
+        return 1; //Exit with an error
+    }
+
+    string filename = argv[1];
+    int s = stoi(argv[2]); //Convert the S value argument from string to int
+
+    //Read integers from the file into a vector
+    vector<int> data;
+    ifstream inputFile(filename);
+    if (!inputFile) {
+        cerr << "Error: Could not open file " << filename << endl;
+        return 1;
+    }
+    int number;
+    while (inputFile >> number) {
+        data.push_back(number);
+    }
+    inputFile.close();
+
+    //Prepare for sorting
     ll comparisons = 0;
-    int s = 4;
 
-    hybridSort(sample, 0, sample.size() - 1, comparisons, s);
+    //Measure the time
+    auto start = chrono::high_resolution_clock::now();
 
-    for (int num : sample)
-        cout << num << " ";
-    cout << endl
-         << comparisons;
+    //Run the hybrid sort algorithm
+    hybridSort(data, 0, data.size() - 1, comparisons, s);
+
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double, milli> duration = end - start; //Duration in milliseconds
+
+    //Print the results
+    //Format: "S,n,comparisons,time_ms"
+    cout << s << "," << data.size() << "," << comparisons << "," << duration.count() << endl;
+
+    return 0;
 }
